342947b9e5ea44c634155a490272b70b4e94bf11
TomasKopunec <tomas.kopunec5@gmail.com>
2023-06-25 19:34:08 +0200

    Initial Commit

---
+++ src/main/java/com/kopunec/springweb/application/Application.java
@@ -0,0 +1,13 @@
+package com.kopunec.springweb.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
