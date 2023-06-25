69cc75b9686d3d46ee82998950ba3ebc8d1ac003
TomasKopunec <tomas.kopunec5@gmail.com>
2023-06-25 20:12:32 +0200

    First Controller

---
+++ src/main/java/com/kopunec/springweb/application/hello/SayHelloController.java
@@ -0,0 +1,26 @@
+package com.kopunec.springweb.application.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("/say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @RequestMapping("/say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        return "<html>" +
+                "<head><title>Spring Boot Application</title></head>" +
+                "<body>" +
+                "<h1>Spring Boot Page</h1>" +
+                "</body>" +
+                "</html>";
+    }
+}
